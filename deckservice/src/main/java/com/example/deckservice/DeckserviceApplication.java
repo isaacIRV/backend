fd1868d70285fabd3a7ae36859464dbbb50a69b6
isaacIRV <i.rivas@duocuc.cl>
2025-11-20 22:43:42 -0300

    supresion de errores

--- deckservice/src/main/java/com/example/deckservice/DeckserviceApplication.java
+++ deckservice/src/main/java/com/example/deckservice/DeckserviceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class deckserviceApplication {
+public class DeckserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeckserviceApplication.class, args);
