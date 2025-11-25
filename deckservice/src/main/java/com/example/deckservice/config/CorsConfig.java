aa01013d3ac1573267f19cb5e0c42c7453ef77b0
isaacIRV <i.rivas@duocuc.cl>
2025-11-24 23:31:26 -0300

    asd

--- deckservice/src/main/java/com/example/deckservice/config/CorsConfig.java
+++ deckservice/src/main/java/com/example/deckservice/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.deckbuilder.config;
+package com.example.deckservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
